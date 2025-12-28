bbdf99e8899c59a3032e0c589bbbacef8f785987
EmileG06 <emilegodin36@gmail.com>
2025-12-28 11:44:48 -0500

    Created premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,94 @@
+project "GLFW"
+        kind "StaticLib"
+        language "C"
+        staticruntime "on"
+        toolset "v145"
+
+        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        flags { "MultiProcessorCompile" }
+
+        files
+        {
+            "include/GLFW/glfw3.h",
+            "include/GLFW/glfw3native.h",
+            "src/internal.h",
+            "src/platform.h",
+            "src/mappings.h",
+            "src/context.c",
+            "src/init.c",
+            "src/input.c",
+            "src/monitor.c",
+            "src/platform.c",
+            "src/vulkan.c",
+            "src/window.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
+            "src/null_platform.h",
+            "src/null_joystick.h",
+            "src/null_init.c",
+            "src/null_monitor.c",
+            "src/null_window.c",
+            "src/null_joystick.c"
+        }
+
+        filter "system:windows"
+            systemversion "latest"
+
+            files
+            {
+                "src/win32_platform.h",
+                "src/win32_joystick.h",
+                "src/win32_init.c",
+                "src/win32_joystick.c",
+                "src/win32_monitor.c",
+                "src/win32_time.h",
+                "src/win32_time.c",
+                "src/win32_thread.h",
+                "src/win32_thread.c",
+                "src/win32_module.c",
+                "src/win32_window.c",
+                "src/wgl_context.c"
+            }
+
+            defines
+            {
+                "_GLFW_WIN32",
+                "_CRT_SECURE_NO_WARNINGS"
+            }
+
+        filter "system:linux"
+            files
+            {
+                "src/x11_platform.h",
+                "src/xkb_unicode.h",
+                "src/posix_time.h",
+                "src/posix_thread.h",
+                "src/glx_context.h",
+                "src/egl_context.h",
+                "src/osmesa_context.h",
+                "src/x11_init.c",
+                "src/x11_monitor.c",
+                "src/x11_window.c",
+                "src/xkb_unicode.c",
+                "src/posix_time.c",
+                "src/posix_thread.c",
+                "src/posix_module.c",
+                "src/glx_context.c",
+                "src/linux_joystick.h",
+                "src/linux_joystick.c"
+            }
+
+            defines
+            {
+                "_GLFW_X11"
+            }
+
+        filter "configurations:Debug"
+            runtime "Debug"
+            symbols "on"
+
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "on"
